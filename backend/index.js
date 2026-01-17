9a92882540fd89d056d2c0ecefa4bbe54485d0a1
Ritesh Tiwari <riteshtiwari9651270995@gmail.com>
2026-01-17 15:48:23 +0530

    update backend cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app=express()
 app.use(cors({
     origin:[
         "http://localhost:5173",
-         "http://localhost:3000"
+         "https://virtual-assistant-frontend-hlng.onrender.com"
         ],
     credentials:true
 }))
